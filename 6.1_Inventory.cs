124a6ae6f7fb459ad2eced55fa86d195e4212c5a
phildh89 <phildh89@gmail.com>
2020-02-20 10:15:37 -0800

    Updated Inventory class

--- 6.1_Inventory.cs
+++ 6.1_Inventory.cs
@@ -2,36 +2,71 @@
 
 public class Inventory
 {
-    int arrayCount = 0;
-    
-    string[] inventory;
-    string Input;
-
-	public Inventory()
-	{
-        inventory[0] = "a";
-        inventory[1] = "b";
-        inventory[2] = "snails";
-
-        Console.Clear();
-        Console.WriteLine("Inventory Test: type inventory below");
-        Input = Console.ReadLine().ToUpper();
-        //Inventory System:
-        if (Input == "INVENTORY" || Input == "INV")
+    public static int GoldAmount;
+    public static int PotatoAmount;
+    public static int UnObtainiumAmount;
+    public static void ShowInventory()      //displays current inventory.
+    {
+        Console.WriteLine("\n\nYour Inventory Contains : "); //TODO: INVENTORY - Replace {name} to call Player's username
+        Console.WriteLine($"\t{GoldAmount} Gold");
+        Console.WriteLine($"\t{PotatoAmount} Potatos");
+        Console.WriteLine($"\t{UnObtainiumAmount} UnObtainium");
+        //TODO: INVENTORY - Add more items here and add Methods
+    }
+    public static void Gold(int itemAmount)
+    {
+        if (GoldAmount + itemAmount < 0)      //Doesn't allow inventory to go negative
         {
-            Console.Clear();
-            Console.WriteLine("Your Inventory Contains : ");
-            for (arrayCount = 0; arrayCount < 20; arrayCount++)
-            {
-                int inventory_position = arrayCount + 1;
-                Console.SetCursorPosition(0, inventory_position);
-                Console.WriteLine("{0}", inventory[arrayCount]);
-            }
+            Console.WriteLine($"{GoldAmount} Gold  {itemAmount}");
+            Console.WriteLine("Not Enough Gold!");
         }
-        else
+        else if (itemAmount >= 0)                 //adding
         {
-
+            Console.WriteLine($"{GoldAmount} Gold  +{itemAmount}");
+            GoldAmount += itemAmount;
+        }
+        else                                    //substracting
+        {
+            Console.WriteLine($"{GoldAmount} Gold  {itemAmount}");
+            GoldAmount += itemAmount;
+        }
+    }
+    public static void Potato(int itemAmount)
+    {
+        if (PotatoAmount + itemAmount < 0)      //Doesn't allow inventory to go negative
+        {
+            Console.WriteLine($"{PotatoAmount} Potatos  {itemAmount}");
+            Console.WriteLine("Not Enough Potatos!");
+        }
+        else if (itemAmount >= 0)                 //adding
+        {
+            Console.WriteLine($"{PotatoAmount} Potatos  +{itemAmount}");
+            PotatoAmount += itemAmount;
+        }
+        else                                    //substracting
+        {
+            Console.WriteLine($"{PotatoAmount} Potatos  {itemAmount}");
+            PotatoAmount += itemAmount;
         }
 
-	}
+    }
+    public static void UnObtainium(int itemAmount)
+    {
+        if (UnObtainiumAmount + itemAmount < 0)      //Doesn't allow inventory to go negative
+        {
+            Console.WriteLine($"{UnObtainiumAmount} UnObtainium  {itemAmount}");
+            Console.WriteLine("Not Enough UnObtainium!");
+        }
+        else if (itemAmount >= 0)                 //adding
+        {
+            Console.WriteLine($"{UnObtainiumAmount} UnObtainium  +{itemAmount}");
+            UnObtainiumAmount += itemAmount;
+        }
+        else                                    //substracting
+        {
+            Console.WriteLine($"{UnObtainiumAmount} UnObtainium  {itemAmount}");
+            UnObtainiumAmount += itemAmount;
+        }
+
+    }
 }
